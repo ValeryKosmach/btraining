69e239220a93f7e46f0d1da20345986235420f0d
ValeryKosmach <vkasmach@gmail.com>
2021-03-14 15:27:09 +0300

    Создание интеллектуальной фикстуры с проверкой валидности

--- fixture/application.py
+++ fixture/application.py
@@ -9,6 +9,13 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
+    def is_valid(self):
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
     def open_home_page(self):
         driver = self.driver
         driver.get("http://localhost:8080/addressbook/group.php")
